3a27e0a1cd0426237db6a1fd99137800dc413c2d
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-12-22 22:20:52 +0100

    improve the content support

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,42 @@
-export default ({ env }) => ({
-    upload1: {
+const {
+    FILE_PROVIDER = "cloudinary",
+    CLOUDINARY_NAME,
+    CLOUDINARY_KEY,
+    CLOUDINARY_SECRET,
+    CF_PUBLIC_ACCESS_URL,
+    CF_ACCESS_KEY_ID,
+    CF_ACCESS_SECRET,
+    CF_ENDPOINT,
+    CF_BUCKET
+} = process.env;
+
+const cloud = {
+    cloudinary: {
+        config: {
+            provider: "cloudinary",
+            providerOptions: {
+                cloud_name: CLOUDINARY_NAME,
+                api_key: CLOUDINARY_KEY,
+                api_secret: CLOUDINARY_SECRET
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            }
+        }
+    },
+    cloudflare: {
         config: {
             provider: "strapi-provider-cloudflare-r2",
             providerOptions: {
-                accessKeyId: env("CF_ACCESS_KEY_ID"),
-                secretAccessKey: env("CF_ACCESS_SECRET"),
-                endpoint: env("CF_ENDPOINT"),
+                accessKeyId: CF_ACCESS_KEY_ID,
+                secretAccessKey: CF_ACCESS_SECRET,
+                endpoint: CF_ENDPOINT,
                 params: {
-                    Bucket: env("CF_BUCKET"),
+                    Bucket: CF_BUCKET,
                 },
-                cloudflarePublicAccessUrl: env("CF_PUBLIC_ACCESS_URL"),
+                cloudflarePublicAccessUrl: CF_PUBLIC_ACCESS_URL,
                 pool: false,
             },
             actionOptions: {
@@ -18,18 +45,17 @@ export default ({ env }) => ({
                 delete: {},
             },
         },
-    },
-    upload: {
-        provider: "cloudinary",
-        providerOptions: {
-            cloud_name: env("CLOUDINARY_NAME"),
-            api_key: env("CLOUDINARY_KEY"),
-            api_secret: env("CLOUDINARY_SECRET")
-        },
-        actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-        },
     }
+};
+
+const upload = (cloud[FILE_PROVIDER] || cloud.cloudflare);
+
+console.log({
+    name: "provider",
+    required: FILE_PROVIDER,
+    loaded: upload?.config?.provider
+});
+
+export default () => ({
+    upload
 });

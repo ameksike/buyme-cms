286a28ed95e93ab631096a1773da5c3483b700fc
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-12-27 12:12:29 +0100

    update the balance calculation

--- src/api/transaction/controllers/transaction.ts
+++ src/api/transaction/controllers/transaction.ts
@@ -5,9 +5,19 @@
 import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::transaction.transaction', ({ strapi }) => ({
-    async getBalance(ctx) {
+    async getBalanceByUser(ctx) {
         try {
-            const result = await strapi.service('api::transaction.transaction').getBalance(ctx.params.id);
+            const result = await strapi.service('api::transaction.transaction').getBalanceByUser(ctx.params.id);
+            const sanitizedResult = await this.sanitizeOutput(result, ctx);
+            ctx.body = sanitizedResult;
+        } catch (err) {
+            console.log(err)
+            ctx.body = err;
+        }
+    },
+    async getBalanceByDoc(ctx) {
+        try {
+            const result = await strapi.service('api::transaction.transaction').getBalanceByDocId(ctx.params.id);
             const sanitizedResult = await this.sanitizeOutput(result, ctx);
             ctx.body = sanitizedResult;
         } catch (err) {
--- src/api/transaction/services/transaction.ts
+++ src/api/transaction/services/transaction.ts
@@ -5,7 +5,7 @@
 import { Core, factories } from '@strapi/strapi';
 
 export default factories.createCoreService('api::transaction.transaction', ({ strapi }: { strapi: Core.Strapi }) => ({
-    async getBalance(documentId) {
+    async getBalanceByUser(documentId) {
         try {
             const res = await strapi.db.connection.raw(`
                 WITH totals AS (
@@ -27,6 +27,48 @@ export default factories.createCoreService('api::transaction.transaction', ({ st
                     credit_total - debit_total AS balance
                 FROM 
                     totals;
+            `, [documentId]);
+            const data = res.rows[0];
+            return data;
+        } catch (error) {
+            return { error: "Error fetching transcript: " + error, data: null };
+        }
+    },
+    async getBalanceByDocId(documentId) {
+        try {
+            const res = await strapi.db.connection.raw(`
+                WITH user_id_from_transaction AS (
+                    SELECT 
+                        u.id AS user_id
+                    FROM 
+                        public.transactions AS t
+                    INNER JOIN 
+                        public.transactions_user_lnk AS tu ON tu.transaction_id = t.id
+                    INNER JOIN 
+                        public.up_users AS u ON u.id = tu.user_id
+                    WHERE 
+                        t.document_id = ?
+                    LIMIT 1
+                ),
+                user_totals AS (
+                    SELECT 
+                        COALESCE(SUM(CASE WHEN t.type = 'Credit' THEN t.value * t.rate ELSE 0 END), 0) AS credit_total,
+                        COALESCE(SUM(CASE WHEN t.type = 'Debit' THEN t.value * t.rate ELSE 0 END), 0) AS debit_total
+                    FROM 
+                        public.transactions AS t
+                    INNER JOIN 
+                        public.transactions_user_lnk AS tu ON tu.transaction_id = t.id
+                    INNER JOIN 
+                        user_id_from_transaction AS uit ON tu.user_id = uit.user_id
+                    WHERE 
+                        t.published_at IS NOT NULL
+                )
+                SELECT 
+                    credit_total AS credit,
+                    debit_total AS debit,
+                    credit_total - debit_total AS balance
+                FROM 
+                    user_totals;
             `, [documentId]);
             const data = res.rows[0];
             return data;
